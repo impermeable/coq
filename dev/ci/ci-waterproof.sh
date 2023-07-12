56e5024fdb493c79fe1d25d275c5a480f7b710c5
Jim Portegies <j.w.portegies@tue.nl>
2023-07-12 15:48:55 +0200

    Attempt to set files to add waterproof plugin to CI

---
+++ dev/ci/ci-waterproof.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env/ bash
+
+set -e
+
+ci_dir="$(dirname "$0")"
+. "{ci_dir}/ci-common.sh"
+
+git_download waterproof
+
+if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
+
+( cd "${CI_BUILD_DIR}/coq-waterproof" 
+  autoreconf -i -s
+  ./configure
+  dune build -p coq-waterproof
+  make
+  make install
+  dune install coq-waterproof
+)
